088608a6320e57f9923dcb74e0e8544f967bd178
dm1tro69 <dimolg22@gmail.com>
2021-10-31 18:30:53 +0200

    add masonry

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Intro from "./components/Intro";
 import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "./store";
 import {getPhotos, setError} from "./store/actions/photosActions";
+import Masonry, {ResponsiveMasonry} from 'react-responsive-masonry'
 
 
 const App:FC = () => {
@@ -26,9 +27,44 @@ const App:FC = () => {
         setTitle(`Search Results For "${query}"`)
 
     }
+
+    let content = null
+    if (loading){
+        content = <div className={'is-flex is-justify-content-center py-6'}>
+            <div className={'loading'}></div>
+        </div>
+    }else {
+        (content = error ? <div className={'notification is-danger mt-6 has-text-centered'}>{error}</div>:
+            <>
+                <h2 className={'is-size-1 has-text-centered py-2'}>{title}</h2>
+                {photos.length > 0
+                    ? <ResponsiveMasonry columnsCountBreakPoints = {{480: 2, 900: 5}}>
+                        <Masonry gutter={20}>
+                            {photos.map(photo => (
+                                <div key={photo.id} className={'masonry-item'}>
+                                    <a href="/#" onClick={(e)=>{}}>
+                                        <img src={photo.src.large} alt="img"/>
+                                    </a>
+                                </div>
+                            ))}
+                        </Masonry>
+                    </ResponsiveMasonry>
+                    : <p className={'has-text-centered'}>No results</p>
+                }
+                <div className={'is-flex is-justify-content-center py-6'}>
+                    {((total_results > page * 10) || mode === 'trending')
+                    && <button className={'button is-primary is-large'}>Load More</button>}
+                </div>
+            </>
+        )
+    }
+
   return (
     <div>
-      <Intro onSearch={() => {}}/>
+      <Intro onSearch={searchPhotosHandler}/>
+        <div className={'container px-4'}>
+            {content}
+        </div>
     </div>
   );
 }
